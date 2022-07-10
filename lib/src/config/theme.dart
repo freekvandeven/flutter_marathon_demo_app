0074df3f581ee4a763609685b06ade01d0066ad0
Freek van de Ven <freek@iconica.nl>
2022-07-10 17:43:20 +0200

    add colors

--- lib/src/config/theme.dart
+++ lib/src/config/theme.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 ThemeData getTheme() {
   return ThemeData(
     backgroundColor: Colors.white,
-    primaryColor: Colors.blue,
+    primaryColor: Color(0xFF1164ea),
+    colorScheme: ColorScheme(
+      brightness: Brightness.light,
+      primary: Color(0xFF1164ea),
+      onPrimary: Color(0xFF4740e8),
+      secondary: Color(0xFFfeca5c),
+      onSecondary: Colors.black,
+      error: Colors.red,
+      onError: Colors.red,
+      background: Colors.white,
+      onBackground: Colors.black,
+      surface: Colors.black,
+      onSurface: Colors.black,
+    ),
   );
 }
--- lib/src/ui/screens/home.dart
+++ lib/src/ui/screens/home.dart
@@ -8,11 +8,41 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: GestureDetector(
-        onTap: () {
-          Navigator.of(context).pushNamed(MarathonRoute.secondScreen.route);
-        },
-        child: Text('screen 1'),
+      body: Stack(
+        children: [
+          // background element
+          Column(
+            children: [
+              Text('New York Marathon'),
+              Text('30:22:39'),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    MarathonRoute.secondScreen.route,
+                  );
+                },
+                child: Text('Playbutton'),
+              ),
+            ],
+          ),
+          DraggableScrollableSheet(
+            builder: (context, scrollController) {
+              return Container(
+                color: Colors.white,
+                child: ListView.builder(
+                  controller: scrollController,
+                  itemCount: 100,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('Item $index'),
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
